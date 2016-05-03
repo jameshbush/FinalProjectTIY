cdf3fe0c5f93350f542b2edc4354995929d95148
J-Henry <JH@JH.local>
2016-05-03 15:50:48 -0400

    Complete user login/signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,7 +6,8 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      redirect_to :home
+      session[:current_user_id] = @user.id
+      redirect_to :root
     else
       render :new
     end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,18 +1,19 @@
 class User < ActiveRecord::Base
 
-  # password
   has_secure_password
   validates :password_digest, presence: true
   validates :password, presence: true,
                        length: { minimum: 6 }
-  # email
+
   EMAIL_REGEX =/\A([\w+\-].?)+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i
   before_validation { self.email.downcase! }
   validates :email, presence: true,
                     uniqueness: true,
                     format: { with: EMAIL_REGEX }
-  # phone
-  before_validation { self.contact_pref.downcase! }
+
+  validates :contact_pref, presence: true
+  before_save { self.contact_pref.downcase! }
+
   before_validation :format_phone
 
   private
