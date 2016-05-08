4c76ef3808d6a0639cc8f02d4262ff92c3c72a70
J-Henry <JH@JH.local>
2016-05-08 11:12:40 -0400

    Move reports helper methods into ReportsHelper module

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,7 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   helper_method :report_due?, :active_journey, :active_report
   include LoginsHelper
+  include ReportsHelper
 
   def home
     render 'pages/home'
@@ -9,13 +10,5 @@ class ApplicationController < ActionController::Base
 
   def active_journey
     @active_journey ||= current_user.current_journey
-  end
-
-  def report_due?
-    !active_journey.reports.find_by(created_at: Time.zone.now.beginning_of_day..Time.zone.now.end_of_day)
-  end
-
-  def active_report
-    active_journey.reports.find_by(created_at: Time.zone.now.beginning_of_day..Time.zone.now.end_of_day)
   end
 end
