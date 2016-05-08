e81a02eaebb675bfff7dd32a8b86f3c2fcafba9b
J-Henry <JH@JH.local>
2016-05-08 11:09:42 -0400

    Move login helper methods to LoginsHelper module

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,31 +1,14 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
-  helper_method :current_user, :logged_in?, :require_user, :disallow_user,
-                :report_due?, :active_journey, :active_report
+  helper_method :report_due?, :active_journey, :active_report
+  include LoginsHelper
 
   def home
     render 'pages/home'
   end
 
-  def logged_in?
-    !current_user.nil?
-  end
-
-  def current_user
-    @current_user ||= session[:current_user_id] &&
-      User.find_by_id(session[:current_user_id])
-  end
-
   def active_journey
     @active_journey ||= current_user.current_journey
-  end
-
-  def require_user
-    redirect_to :root unless current_user
-  end
-
-  def disallow_user
-    redirect_to :root if current_user
   end
 
   def report_due?
--- app/helpers/logins_helper.rb
+++ app/helpers/logins_helper.rb
@@ -1,2 +1,18 @@
 module LoginsHelper
+  def logged_in?
+    !current_user.nil?
+  end
+
+  def current_user
+    @current_user ||= session[:current_user_id] &&
+      User.find_by_id(session[:current_user_id])
+  end
+
+  def require_user
+    redirect_to :root unless current_user
+  end
+
+  def disallow_user
+    redirect_to :root if current_user
+  end
 end
