d5929a658fe81e83d853cf6bfdf2372d59527cc8
J-Henry <JH@JH.local>
2016-05-08 11:15:52 -0400

    Move active_journey method into JourneysHelper module

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,14 +1,10 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
-  helper_method :report_due?, :active_journey, :active_report
   include LoginsHelper
   include ReportsHelper
+  include JourneysHelper
 
   def home
     render 'pages/home'
-  end
-
-  def active_journey
-    @active_journey ||= current_user.current_journey
   end
 end
--- app/helpers/journeys_helper.rb
+++ app/helpers/journeys_helper.rb
@@ -1,2 +1,5 @@
 module JourneysHelper
+  def active_journey
+    @active_journey ||= current_user.current_journey
+  end
 end
