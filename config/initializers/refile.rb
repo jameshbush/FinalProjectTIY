28ed305162bbab88f83cd171cb650a765ed7bf3f
J-Henry <JH@JH.local>
2016-05-09 16:47:46 -0400

    Adjusts refile config

--- config/initializers/refile.rb
+++ config/initializers/refile.rb
@@ -1,10 +1,10 @@
 require "refile/s3"
 
 aws = {
-  access_key_id: ENV['AWS-ACCESS-KEY'],
-  secret_access_key: ENV['AWS-SECRET-KEY'],
-  region: ENV['AWS-REGION'],
-  bucket: ENV['AWS-BUCKET'],
+  access_key_id: ENV['AWS_ACCESS_KEY'],
+  secret_access_key: ENV['AWS_SECRET_KEY'],
+  region: ENV['AWS_REGION'],
+  bucket: ENV['AWS_BUCKET'],
 }
 Refile.cache = Refile::S3.new(prefix: "cache", **aws)
 Refile.store = Refile::S3.new(prefix: "store", **aws)
