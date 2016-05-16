791d2f08e9349758481422f0a446f70d88c5986a
J-Henry <JH@JH.local>
2016-05-16 17:15:53 -0400

    Setup Sendgridd Inbound Parse API. Users submit report in email

---
+++ lib/email_processor.rb
@@ -0,0 +1,17 @@
+class EmailProcessor
+  def initialize(email)
+    @email = email
+  end
+
+  def process
+    user = User.find_by_email(@email.from[:email])
+    @report = user.current_journey.reports.find_or_initialize_by(
+    created_at: Time.zone.now.beginning_of_day..Time.zone.now.end_of_day)
+    img = @email.attachments.first
+    txt = @email.body
+
+    @report.image  = img if img
+    @report.survey = txt if txt
+    @report.save!
+  end
+end
