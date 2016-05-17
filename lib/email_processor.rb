eabd739efb80b1640142bb735b870ac59f8992b7
J-Henry <JH@JH.local>
2016-05-17 10:23:56 -0400

    Email reply updates report

--- lib/email_processor.rb
+++ lib/email_processor.rb
@@ -9,7 +9,7 @@ class EmailProcessor
     created_at: Time.zone.now.beginning_of_day..Time.zone.now.end_of_day)
     img = @email.attachments.first
     txt = @email.body
-
+    Rails.logger.info @email.inspect
     @report.image  = img if img
     @report.survey = txt if txt
     @report.save!
