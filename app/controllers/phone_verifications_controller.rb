679b6460ab4c730bbc14f9b54da0337e354dcb61
J-Henry <JH@JH.local>
2016-05-17 16:18:55 -0400

    remove debugging, fix #32

--- app/controllers/phone_verifications_controller.rb
+++ app/controllers/phone_verifications_controller.rb
@@ -5,14 +5,12 @@ class PhoneVerificationsController < ApplicationController
 
   def create
     response = Authy::API.verify(:id => current_user.authy_id, :token => params[:token][:token])
-    puts session.inspect
 
     if response.ok?
       current_user.phone_verified = true
       current_user.save!
       flash[:success] = "Phone number #{current_user.cellphone} was verified."
       redirect_to user_path(current_user)
-
     else
       flash.now[:warning] = "Please enter the number texted to #{current_user.cellphone}."
       render :new
