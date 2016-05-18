df0e39f5394f8ea69257c7aa6ab3bc8ce488d140
J-Henry <JH@JH.local>
2016-05-18 16:23:51 -0400

    Fix parse text

--- app/models/report.rb
+++ app/models/report.rb
@@ -4,9 +4,10 @@ class Report < ActiveRecord::Base
   validates_inclusion_of :survey, in: 1..10, allow_blank: true
 
   def self.parse_text(text)
-    words = text.split.downcase!
+    words = text.split
     surveys = Hash.new
     words.each_with_index do |word, index|
+      word.downcase!
       if word == 'b' || word == 'before'
         surveys[:before] = words[index + 1]
       elsif word == 'a' || word == 'after'
