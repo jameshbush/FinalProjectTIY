6e3f8eec3fc71e84b061e081d1544f9299728d2b
J-Henry <JH@JH.local>
2016-05-19 11:18:47 -0400

    Add homepage signup button

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,7 @@
 class PagesController < ApplicationController
+  skip_before_action :require_email_confirmation
+  skip_before_action :require_quest
+  rescue_from ActiveRecord::RecordNotFound, with: :dude_wheres_my_record
 
   def home
     render 'home'
