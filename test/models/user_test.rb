6df402b742476e56fa8aa40f5e2c4ea1a70e2d6a
James <jamesbvsh@gmail.com>
2016-06-19 18:40:49 -0400

    Add user model tests

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,4 +1,35 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
+  def setup
+    @user = User.find(1)
+  end
+
+  test "user has name" do
+    assert @user.name == "jamie", "Username incorrect"
+  end
+
+  test "user has email" do
+    assert @user.email == "jamie@acme.org", "User email incorrect"
+  end
+
+  test "user has cellphone" do
+    assert @user.cellphone == "+15555555555", "User cellphone incorrect"
+  end
+
+  test "user has contact_pref" do
+    assert @user.contact_pref == "phone", "User contact_pref incorrect"
+  end
+
+  test "user has phone_verified" do
+    assert @user.phone_verified == true, "User phone_verified not working"
+  end
+
+  test "user has email_verified" do
+    assert @user.email_verified == true, "User email_verified not working"
+  end
+
+  test "user has password_digest" do
+    refute @user.password_digest.blank?, "User password_digest not working"
+  end
 end
