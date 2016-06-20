fb82823a0be6dd08aed59cbdbdd99d7956d78db5
James <jamesbvsh@gmail.com>
2016-06-20 16:47:19 -0400

    Add user test coverage

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -3,6 +3,7 @@ require 'test_helper'
 class UserTest < ActiveSupport::TestCase
   def setup
     @user = User.find(1)
+    @u2 = User.find(2)
   end
 
   test "user has name" do
@@ -31,5 +32,36 @@ class UserTest < ActiveSupport::TestCase
 
   test "user has password_digest" do
     refute @user.password_digest.blank?, "User password_digest not working"
+  end
+
+  test "user password length min is 6 chars" do
+    @user.password = @user.password_confirmation = "j" * 5
+    assert_not @user.valid?
+  end
+
+  test "user needs valid email" do
+    @user.email = "hello@world"
+    assert_not @user.valid?
+    @user.email = ""
+    assert_not @user.valid?
+  end
+
+  test "user needs unique email" do
+    @user.email = @u2.email
+    assert_not @user.valid?
+  end
+
+  test "user email addresses gets downcased" do
+    mixed_case_email = "Foo@ExAMPle.CoM"
+    @user.email = mixed_case_email
+    @user.save
+    assert_equal mixed_case_email.downcase, @user.reload.email
+  end
+
+  test "user cellphone gets formatted" do
+    humanized_cellphone = "(555)555-5555"
+    @user.cellphone = humanized_cellphone
+    @user.save
+    assert_equal "+15555555555", @user.reload.cellphone
   end
 end
